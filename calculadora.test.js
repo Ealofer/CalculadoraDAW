ffbb5d92ed50a64d4e27c44397054ac966e77c5e
ealofer526 <ealofer526@g.educaand.es>
2025-02-10 11:16:15 +0100

    mas updates

--- calculadora.test.js
+++ calculadora.test.js
@@ -7,3 +7,9 @@ test('Suma 2 + 3 y devuelve 5', () => {
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
 });
+test('Multiplica 5 * 3 y devuelve 15', () => {
+    expect(multiplicar(5, 3)).toBe(15);
+});
+test('Divide 10 / 2 y devuelve 5', () => {
+    expect(dividir(10, 2)).toBe(5);
+});
