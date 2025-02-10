e4c01a726af9dc15e380bb0feb07b0d7b3c3a0fc
ealofer526 <ealofer526@g.educaand.es>
2025-02-10 11:25:42 +0100

    ddd

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,22 @@
-// calculadora.js
+function suma(a, b) {    
+  return a + b; 
+} 
 
-function sumar(a, b) {
-  return a + b;
- }
- function restar(a, b) {
-  return a - b;
- }
- function multiplicar(a, b) {
-  return a * b;
- }
- function dividir(a, b) {
-  if (b === 0) {
-  throw new Error("No se puede dividir por cero");
-  }
-  return a / b;
- }
- // Hacer las funciones accesibles en el navegador
+function resta(a, b) {    
+  return a - b; 
+} 
 
- module.exports=(sumar,restar,multiplicar,dividir);
+function multiplicar(a, b) {    
+  return a * b; 
+} 
+
+function dividir(a, b) {    
+  if (b === 0) {        
+    throw new Error("No se puede dividir por cero");    
+  }    
+  return a / b; 
+}
+
+  // Hacer las funciones accesibles en el navegador 
+
+module.exports = {suma, resta, multiplicar, dividir};
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,15 +1,17 @@
-const { sumar, restar, multiplicar, dividir } = require('./calculadora');
+const { suma, resta, multiplicar, dividir} = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
-    expect(sumar(2, 3)).toBe(5);
+    expect(suma(2, 3)).toBe(5);
 });
 
 test('Resta 5 - 3 y devuelve 2', () => {
-    expect(restar(5, 3)).toBe(2);
+    expect(resta(5, 3)).toBe(2);
 });
-test('Multiplica 5 * 3 y devuelve 15', () => {
-    expect(multiplicar(5, 3)).toBe(15);
+
+test('multiplicar 5 * 2 y devuelve 10', () => {
+    expect(multiplicar(5,2)).toBe(10);
 });
-test('Divide 10 / 2 y devuelve 5', () => {
-    expect(dividir(10, 2)).toBe(5);
+
+test('dividir 10 / 5 y devuelve 2', () => {
+    expect(dividir(10,5)).toBe(2);
 });
